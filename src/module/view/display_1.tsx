a6cb1da6c3a4b238247ac2de92dfb14927cd7d11
lukman <lukman@lukmans-MacBook-Air.local>
2024-03-08 11:19:25 +0800

    update : display 1

--- src/module/view/display_1.tsx
+++ src/module/view/display_1.tsx
@@ -1,39 +1,83 @@
 "use client";
-import { Box, Flex, Grid, GridCol, SimpleGrid, Text } from "@mantine/core";
-import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridCol,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 export default function Display1() {
+  const [isLink, setLink] = useState("https://raven-stone2.wibudev.com/");
+  const router = useRouter();
+
+  function OnLink() {
+    if (isLink == "https://ninox-fox.wibudev.com/") {
+      setLink("https://raven-stone2.wibudev.com/");
+    } else {
+      setLink("https://ninox-fox.wibudev.com/");
+    }
+  }
   return (
     <>
-      {/* <Flex direction={'row'} >
-      <Flex w={"70%"} bg={"red"} h={"100vh"}>
-sdsds
-      </Flex>
-      <Flex w={"30%"} bg={"blue"} h={"100vh"}>
-        <Flex direction={"column"} bg={"green"} h={"50%"} w={"100%"}>
-        </Flex>
-        <Flex direction={"column"} bg={"green"} h={"50%"} w={"100%"}>
-        </Flex>
-      </Flex>
-    </Flex> */}
-      <Grid>
-        <Grid.Col span={{ base: 12, md: 8, lg: 8 }}>
-          <Box h={"100vh"} bg={"blue"}></Box>
-        </Grid.Col>
-        <Grid.Col span={{ base: 12, md: 4, lg: 4 }}>
-          <SimpleGrid
-            cols={{ base: 1, sm: 1, lg: 1 }}
+      <Flex direction={"row"}>
+        <Flex w={"70%"} bg={"red"} h={"100vh"} direction={"column"}>
+          <Group
+            justify="flex-end"
+            p={10}
+            style={{
+              position: "absolute",
+              justifyItems: "flex-end",
+              justifyContent: "flex-end",
+              alignContent: "flex-end",
+              alignItems: "flex-end",
+            }}
           >
-            <Box bg={"red"} h={"50vh"}>
-
+            <Box>
+              <Button onClick={OnLink}>SWITCH</Button>
             </Box>
-            <Box bg={"red"} h={"49vh"}>
+          </Group>
+          {isLink == "https://ninox-fox.wibudev.com/" && (
+            <iframe
+              src={"https://ninox-fox.wibudev.com/"}
+              width={"100%"}
+              height={"100%"}
+            />
+          )}
 
+          {isLink == "https://raven-stone2.wibudev.com/" && (
+            <iframe
+              src={"https://raven-stone2.wibudev.com/"}
+              width={"100%"}
+              height={"100%"}
+            />
+          )}
+        </Flex>
+        <Box w={"30%"} bg={"green"} h={"100vh"}>
+          <Box>
+            <Box h={"50vh"} bg={"blue"}>
+              <iframe
+                src="https://noc.wibudev.com/noc/n1"
+                width={"100%"}
+                height={"100%"}
+              />
             </Box>
-
-          </SimpleGrid>
-        </Grid.Col>
-      </Grid>
+            <Box h={"50vh"} bg={"pink"}>
+              <iframe
+                src="https://vladmandic.github.io/human/demo/"
+                width={"100%"}
+                height={"100%"}
+              />
+            </Box>
+          </Box>
+        </Box>
+      </Flex>
     </>
   );
 }
